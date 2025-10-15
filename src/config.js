a4465a49bc4968cad3d82fc8c6bed566a68176ab
YoussefAshraf832 <YoussefAshraf832@gmail.com>
2025-10-15 12:37:12 +0300

    first-edit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const URL = `https://cashly-back.vercel.app/api/v1`;
+const URL = `https://cashly-back-production.up.railway.app/api/v1`;
+// const URL = `https://cashly-back.vercel.app/api/v1`;
 // const URL = `http://127.0.0.1:3000/api/v1`;
 
 export { URL };
